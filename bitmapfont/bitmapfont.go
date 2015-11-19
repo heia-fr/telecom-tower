252ad0224192028edc2ea189b93f4f32fe4df2e7
Jacques Supcik <jacques.supcik@hefr.ch>
2015-11-19 12:18:44 +0100

    Cosmetic change

--- bitmapfont/bitmapfont.go
+++ bitmapfont/bitmapfont.go
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//
 // package to render bitmap fonts
+//
 package bitmapfont
 
 type Font struct {
