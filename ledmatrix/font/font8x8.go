acbd7fc10773452e53fadea58c42011c4dfb3d44
Jacques Supcik <jacques.supcik@hefr.ch>
2015-12-29 16:28:20 +0100

    Add credit to Benedikt K. for the font files

--- ledmatrix/font/font8x8.go
+++ ledmatrix/font/font8x8.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Credit: Thank you Benedikt K. for the original bitmap font files.
+//         http://www.mikrocontroller.net/topic/54860
+
 // Bitmap font 8x8
 package font
 
